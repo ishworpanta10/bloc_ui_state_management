8f66c383ce93b2e081de3cf90f346e53e2051b91
ishworpanta10 <ishworpanta10@gmail.com>
2021-07-26 19:14:54 +0545

    added linter and ready to go 🔥

---
+++ lib/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'screens/screens.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
